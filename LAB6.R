0f143b8cbd22a36c01ae334cddac2b224454b942
Gustavo de Leon <gdeleontobias@gmail.com>
2020-09-03 17:21:10 -0600

    INICIO DE R

---
+++ LAB6.R
@@ -0,0 +1,10 @@
+# Instalaci?n de paquetes necesarios para procesar datos:
+# load twitter library - the rtweet library is recommended now over twitteR
+library(rtweet)
+# plotting and pipes - tidyverse!
+library(ggplot2)
+library(dplyr)
+# text mining library
+library(tidytext)
+
+
